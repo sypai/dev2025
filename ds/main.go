de61789ea8cc4ba0b5e4817e58686aa8beb7a436
Suyash Bajpai <suyashbajpai.dev@gmail.com>
2025-04-28 10:22:46 +0530

    commit-ment issues

--- ds/main.go
+++ ds/main.go
@@ -1,14 +1,15 @@
 package main
 
-import ( 
-	"fmt"
+import (
 	"ds/graphs"
+	"fmt"
 )
 
-func main(){
-	fmt.Println("Here we go again!\n\n")
+func main() {
+	fmt.Println("Here we go again!")
+	fmt.Println()
 
-    socialNetwork := graphs.NewGraph(false)
+	socialNetwork := graphs.NewGraph(false)
 
 	// Adding People
 	socialNetwork.AddVertex("Suyash")
@@ -18,11 +19,24 @@ func main(){
 	socialNetwork.AddVertex("Utkarsh")
 
 	// Adding Connections (edges)
-	socialNetwork.AddEdge("Suyash", "Aakshita") 
+	socialNetwork.AddEdge("Suyash", "Aakshita")
 	socialNetwork.AddEdge("Shriya", "Sherlock")
 	socialNetwork.AddEdge("Shriya", "Utkarsh")
 	socialNetwork.AddEdge("Utkarsh", "Suyash")
 
 	socialNetwork.PrintGraph()
-	
+
+	website_nav := graphs.NewGraph(true)
+
+	website_nav.AddVertex("Home")
+	website_nav.AddVertex("About")
+	website_nav.AddVertex("Contact")
+	website_nav.AddVertex("Blog")
+
+	website_nav.AddEdge("Home", "About")
+	website_nav.AddEdge("Home", "Blog")
+	website_nav.AddEdge("About", "Contact")
+	website_nav.AddEdge("Blog", "Contact")
+
+	website_nav.PrintGraph()
 }

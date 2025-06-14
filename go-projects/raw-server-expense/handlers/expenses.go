221988d8b08484f247a206678b1a850d59789304
Suyash Bajpai <suyashbajpai.dev@gmail.com>
2025-06-15 04:05:59 +0530

    [go-projects][raw-server-expense] handling GET, POST expenses

--- go-projects/raw-server-expense/handlers/expenses.go
+++ go-projects/raw-server-expense/handlers/expenses.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"basic-expense-tracker/models"
 	"basic-expense-tracker/store"
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -28,10 +30,32 @@ func (h *ExpenseHandler) HandleExpenses(w http.ResponseWriter, r *http.Request) 
 
 // /GET expenses
 func (h *ExpenseHandler) HandleGETExpenses(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
+	err := json.NewEncoder(w).Encode(h.DB.Expenses)
+	if err != nil {
+		http.Error(w, "Failed to encode expenses", http.StatusInternalServerError)
+		return
+	}
 }
 
 // /POST expenses
 func (h *ExpenseHandler) HandlePOSTExpense(w http.ResponseWriter, r *http.Request) {
+	var expense models.Expense
 
+	// Decode JSON request body into Expense struct
+	err := json.NewDecoder(r.Body).Decode(&expense)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	// Assign a new ID and append to DB
+	expense.ID = h.DB.NextID
+	h.DB.NextID++
+	h.DB.Expenses = append(h.DB.Expenses, expense)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(expense)
 }
